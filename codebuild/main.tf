ed7b5c2f670bcc75388f94ccf75dae6a0d5aade0
trevs-social@outlook.com <trevs-social@outlook.com>
2020-05-23 18:19:56 +0100

    Modified to use locals and fix tpl refs

--- codebuild/main.tf
+++ codebuild/main.tf
@@ -2,18 +2,22 @@ provider "aws" {
   region = var.aws_region
 }
 
+locals {
+  unique_name = "${var.repo_name}-${var.codebuild_purpose}"
+}
+
 # CodeBuild IAM Permissions
 data "template_file" "codebuild_assume_role_policy_template" {
-  template = file("./policies/codebuild_assume_role.tpl")
+  template = file("${path.module}/policies/codebuild_assume_role.tpl")
 }
 
 resource "aws_iam_role" "codebuild_assume_role" {
-  name               = "${var.repo_name}-${var.codebuild_purpose}-codebuild-role"
+  name               = "${local.unique_name}-codebuild-role"
   assume_role_policy = data.template_file.codebuild_assume_role_policy_template.rendered
 }
 
 data "template_file" "codebuild_policy_template" {
-  template = file("./policies/codebuild.tpl")
+  template = file("${path.module}/policies/codebuild.tpl")
   vars = {
     artifact_bucket   = var.build_artifact_bucket_arn
     aws_kms_key       = var.artifact_encryption_key_arn
@@ -22,14 +26,14 @@ data "template_file" "codebuild_policy_template" {
 }
 
 resource "aws_iam_role_policy" "codebuild_policy" {
-  name   = "${var.repo_name}-${var.codebuild_purpose}-codebuild-policy"
+  name   = "${local.unique_name}-codebuild-policy"
   role   = aws_iam_role.codebuild_assume_role.id
   policy = data.template_file.codebuild_policy_template.rendered
 }
 
 # CodeBuild Project
 resource "aws_codebuild_project" "codebuild_project" {
-  name           = "${var.repo_name}-${var.codebuild_purpose}"
+  name           = local.unique_name
   description    = "The CodeBuild project for ${var.repo_name} (${var.codebuild_purpose})"
   service_role   = aws_iam_role.codebuild_assume_role.arn
   build_timeout  = var.build_timeout
@@ -53,8 +57,8 @@ resource "aws_codebuild_project" "codebuild_project" {
 
   logs_config {
     cloudwatch_logs {
-      group_name  = "${var.repo_name}-${var.codebuild_purpose}-log-group"
-      stream_name = "${var.repo_name}-${var.codebuild_purpose}-log-stream"
+      group_name  = "${local.unique_name}-codebuild-log-group"
+      stream_name = "${local.unique_name}-codebuild-log-stream"
     }
   }
 }
