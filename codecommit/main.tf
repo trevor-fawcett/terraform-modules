009d8313185e7e7add1cc1a950d64ac085d671af
trevs-social@outlook.com <trevs-social@outlook.com>
2020-05-24 13:32:23 +0100

    prevent_destroy prevents too much!

--- codecommit/main.tf
+++ codecommit/main.tf
@@ -6,8 +6,4 @@ resource "aws_codecommit_repository" "repo" {
   repository_name = var.repo_name
   description     = "${var.repo_name} repository."
   default_branch  = var.repo_default_branch
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
