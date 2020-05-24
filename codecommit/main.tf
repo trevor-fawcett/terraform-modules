fbf33aac8158ad0282b73a5b42bac76b0789c27e
trevs-social@outlook.com <trevs-social@outlook.com>
2020-05-24 13:40:09 +0100

    attempt to initialise repo

--- codecommit/main.tf
+++ codecommit/main.tf
@@ -6,4 +6,24 @@ resource "aws_codecommit_repository" "repo" {
   repository_name = var.repo_name
   description     = "${var.repo_name} repository."
   default_branch  = var.repo_default_branch
+
+  provisioner "local-exec" {
+    command = "git clone ${aws_codecommit_repository.repo.clone_url_http}"
+  }
+  
+  provisioner "local-exec" {
+    command = "cd ${var.repo_name}"
+  }
+  
+  provisioner "local-exec" {
+    command = "echo '# ${var.repo_name}' > readme.md"
+  }
+  
+  provisioner "local-exec" {
+    command = "git commit -a -m 'initial'"
+  }
+  
+  provisioner "local-exec" {
+    command = "git push -u origin ${var.repo_default_branch}"
+  }
 }
