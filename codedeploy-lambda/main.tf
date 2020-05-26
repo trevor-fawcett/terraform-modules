f13dbf64f460c1289b8c3c1150b1fda148f7a77b
trevs-social@outlook.com <trevs-social@outlook.com>
2020-05-26 23:16:06 +0100

    CodeDeploy amendments

--- codedeploy-lambda/main.tf
+++ codedeploy-lambda/main.tf
@@ -12,28 +12,29 @@ resource "aws_iam_role" "codedeploy_assume_role" {
   assume_role_policy = data.template_file.codedeploy_assume_role_policy_template.rendered
 }
 
-# data "template_file" "codedeploy_policy_template" {
-#   template = file("${path.module}/policies/codedeploy.tpl")
-#   vars = {
-#     artifact_bucket   = var.build_artifact_bucket_arn
-#     aws_kms_key       = var.artifact_encryption_key_arn
-#   }
-# }
-
 resource "aws_iam_role_policy_attachment" "codedeploy_policy" {
   role       = aws_iam_role.codedeploy_assume_role.id
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRoleForLambda"
 }
 
-# CodeDeploy (Lambda)
+# CodeDeploy Configuration (Lambda)
 resource "aws_codedeploy_app" "codedeploy_application" {
   name             = var.repo_name
   compute_platform = "Lambda"
 }
 
+resource "aws_codedeploy_deployment_config" "codedeploy_deployment_config" {
+  deployment_config_name = "${var.repo_name}-deployment-config"
+  compute_platform       = "Lambda"
+
+  traffic_routing_config {
+    type = "AllAtOnce"
+  }
+}
+
 resource "aws_codedeploy_deployment_group" "codedeploy_deployment_group" {
   app_name               = aws_codedeploy_app.codedeploy_application.name
-  deployment_config_name = "CodeDeployDefault.LambdaAllAtOnce"
-  deployment_group_name  = var.repo_name
+  deployment_config_name = aws_codedeploy_deployment_config.codedeploy_deployment_config.id
+  deployment_group_name  = "${var.repo_name}-deployment-group"
   service_role_arn       = aws_iam_role.codedeploy_assume_role.arn
 }
